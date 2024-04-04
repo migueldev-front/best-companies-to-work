890f4f0f28800dc27ecabf017ee9457bc63c510d
miguellyon <miguel.lyon82@gmail.com>
2024-04-04 15:01:49 -0300

    Initial commit with minimal template from https://github.com/lewagon/rails-templates

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,6 @@
 require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-# require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module BestCompaniesToWork
   class Application < Rails::Application
+    config.action_controller.raise_on_missing_callback_actions = false if Rails.version >= "7.1.0"
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework :test_unit, fixture: false
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
