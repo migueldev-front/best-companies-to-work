85abc079c7f77ac71fafd53fde8d391f77bbea81
miguellyon <miguel.lyon82@gmail.com>
2024-05-08 14:25:35 -0300

    more changes in app files

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -14,13 +14,34 @@ class CompaniesController < ApplicationController
 
   def create
     @companies = Company.new(company_params)
-   
+
 
     if @companies.save
       redirect_to companies_path, notice: "Company was created"
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @companies = Company.find(params[:id])
+  end
+
+  def update
+    @companies = Company.find(params[:id])
+
+    if @companies.update(company_params)
+      redirect_to @companies
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @companies = Company.find(params[:id])
+    @companies.destroy
+
+    redirect_to root_path
   end
 
 
